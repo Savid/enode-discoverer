95ab7a1c6eb075cae8e38645405b876c8878600f
Andrew Davis <1709934+Savid@users.noreply.github.com>
2022-09-11 15:01:57 +1000

    fix(ci): publish trigger

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-import logger from '@savid/logger';
-import app from '#app/app';
-logger.info('starting enode discoverer service');
-app().catch((err) => {
-    logger.error('app error', { error: err.toString(), stack: err.stack });
-});
-//# sourceMappingURL=index.js.map
